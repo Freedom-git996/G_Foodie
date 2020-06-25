9fc38064e98544e88af1abc34b7404855784c22e
Freedom-git996 <3128877205@qq.com>
2020-06-25 17:38:49 +0800

    整合Swagger2

---
+++ foodie-api/src/main/java/com/vectory/config/Swagger2.java
@@ -0,0 +1,36 @@
+package com.vectory.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class Swagger2 {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.vectory.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Java架构师养成计划Api")
+                .version("1.0")
+                .description("专注于Java架构师养成")
+                .contact(new Contact("vectory", "http://localhost:8088", "xxxx@168.com"))
+                .build();
+    }
+}
