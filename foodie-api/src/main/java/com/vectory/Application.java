998d1d6434f47918f1653ebff76c3ce4dbf5f741
Freedom-git996 <3128877205@qq.com>
2020-06-25 16:52:35 +0800

    配置唯一ID生成器，并配置组件扫描

--- foodie-api/src/main/java/com/vectory/Application.java
+++ foodie-api/src/main/java/com/vectory/Application.java
@@ -3,9 +3,11 @@ package com.vectory;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.vectory.mapper")
+@ComponentScan(basePackages = {"com.vectory", "org.n3r.idworker"})
 public class Application {
 
     public static void main(String[] args) {
