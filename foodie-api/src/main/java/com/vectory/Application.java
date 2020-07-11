83316d3f4b610cdd783b157cedef982e2cca5f0d
Freedom-git996 <3128877205@qq.com>
2020-07-11 17:42:34 +0800

    开启定时任务，定时关单

--- foodie-api/src/main/java/com/vectory/Application.java
+++ foodie-api/src/main/java/com/vectory/Application.java
@@ -4,10 +4,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.vectory.mapper")
 @ComponentScan(basePackages = {"com.vectory", "org.n3r.idworker"})
+@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
